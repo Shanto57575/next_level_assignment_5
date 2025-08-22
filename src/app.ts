602c2fc33d10a998cc220545cca011a1a58a3957
Md. Shahidul Islam <shanto57575@gmail.com>
2025-08-22 20:48:54 +0600

    cors functionality added

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@ import express, { Request, Response } from "express";
 import { notFound } from "./app/middlewares/notFound";
 import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import { router } from "./app/router";
+import cors from "cors";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
